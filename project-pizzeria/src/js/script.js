5e8a6b07b3e239225ed6d4781b1c90e925e4cf7d
Mateusz Barej <mateusz.barej89@gmail.com>
2023-07-10 12:40:41 +0200

    add instance

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -3,9 +3,9 @@
 {
   'use strict';
 
-const select = {
-  templateOf: {
-    menuProduct: "#template-menu-product",
+  const select = {
+    templateOf: {
+      menuProduct: '#template-menu-product',
     },
     containerOf: {
       menu: '#product-list',
@@ -52,7 +52,20 @@ const select = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(){
+      const thisProduct = this;
+
+      console.log('new Product:', thisProduct);
+    }
+  }
+
   const app = {
+    initMenu: function(){
+      const testProduct = new Product();
+      console.log('testProduct', testProduct);
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,8 +73,13 @@ const select = {
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initMenu();
     },
   };
+
+  const testProduct = new Product();
+  console.log('testProduct', testProduct);
 
   app.init();
 }
